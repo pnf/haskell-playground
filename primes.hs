29300b75e7bc1b98ebd28426c3b20840c40e91ca
Peter Fraenkel <pnf@podsnap.com>
2014-05-02 16:41:16 -0400

    Speed up seive by incrementing by two; now only 1.5x r9ml. Add even simpler and faster version from interwebs. Add a "standard" solution.

--- primes.hs
+++ primes.hs
@@ -24,17 +24,19 @@ crossOff nps =
       (np,ps) = M.findMin nps
       nps'    = M.deleteMin nps
       roll :: Int -> Int -> (M.Map Int IntSet) ->  (M.Map Int IntSet)
-      roll np p nps = M.insertWith IS.union (np+p) (IS.singleton p) nps
+      roll np p nps = M.insertWith IS.union (np+(2*p)) (IS.singleton p) nps
 -- | Lazy list of prime numbers
-seive :: [Int]
-seive = seive' M.empty 2
+seive ::  [Int]
+seive = 2 : seive' M.empty 3
         where
           seive' :: (M.Map Int IntSet) -> Int -> [Int]
           seive' nps n
             | M.null nps || n < (fst . M.findMin $ nps)
-                 = n:seive' (M.insert (2*n) (IS.singleton n) nps) (n+1)
-            | otherwise = seive' (crossOff nps) (n+1)
+                 = n:seive' (M.insert (3*n) (IS.singleton n) nps) (n+2)
+            | otherwise = seive' (crossOff nps) (n+2)
 
+
+-- r0ml's solution, which is 1.5x faster
 
 minus (x:xs) (y:ys) = case (compare x y) of 
           LT -> x : minus  xs  (y:ys)
@@ -59,3 +61,38 @@ primes = 2 : ([3,5..] `minus` unionAll [[p*p,p*p+2*p..] | p <- primes'])
 
 primes' = 3 : ([5,7..] `minus` unionAll [[p*p,p*p+2*p..] | p <- primes'])   
 
+
+-- Matthew Avant's non-seive solution
+
+ma_primes :: Integral a => [a]
+ma_primes = 2 : 3 : 5 : filter ma_prime [7..]
+
+ma_prime :: Integral a => a -> Bool
+ma_prime x = all (divides x) $ primesBelowSqrt x
+
+primesBelowSqrt :: Integral a => a -> [a]
+primesBelowSqrt = flip takeWhile ma_primes . (>=) . ceiling . sqrt . fromIntegral
+
+divides :: Integral a => a -> a -> Bool
+divides x y = 0 /= x `mod` y
+
+
+-- http://en.literateprograms.org/Sieve_of_Eratosthenes_(Haskell)
+merge :: (Ord a) => [a] -> [a] -> [a]
+merge xs@(x:xt) ys@(y:yt) = 
+  case compare x y of
+    LT -> x : (merge xt ys)
+    EQ -> x : (merge xt yt)
+    GT -> y : (merge xs yt)
+diff :: (Ord a) => [a] -> [a] -> [a]
+diff xs@(x:xt) ys@(y:yt) = 
+  case compare x y of
+    LT -> x : (diff xt ys)
+    EQ -> diff xt yt
+    GT -> diff xs yt
+primes2, nonprimes2 :: [Integer]
+primes2    = [2, 3, 5] ++ (diff [7, 9 ..] nonprimes2) 
+nonprimes2 = foldr1 f $ map g $ tail primes2
+  where 
+    f (x:xt) ys = x : (merge xt ys)
+    g p         = [ n * p | n <- [p, p + 2 ..]]
